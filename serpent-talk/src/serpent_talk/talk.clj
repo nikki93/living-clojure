04de173df9406de6928dea6868ea2d79be53d67f
nikki93 <s.nikhilesh@gmail.com>
2016-12-26 23:51:51 -0800

    finish chapter 5

--- serpent-talk/src/serpent_talk/talk.clj
+++ serpent-talk/src/serpent_talk/talk.clj
@@ -1,6 +1,8 @@
-(ns serpent-talk.talk)
+(ns serpent-talk.talk
+  (:require [camel-snake-kebab.core :as csk]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn serpent-talk [input]
+  (str "Serpent!  You said: " (csk/->snake_case input)))
+
+(defn -main [& args]
+  (println (serpent-talk (first args))))
--- serpent-talk/test/serpent_talk/talk_test.clj
+++ serpent-talk/test/serpent_talk/talk_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [serpent-talk.talk :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest test-serpent-talk
+  (testing "Cries 'serpent!' with a snake_case version of the input"
+    (is (= "Serpent!  You said: hello_there"
+           (serpent-talk "hello there")))))
