2f318ae12a42b276684207707a707907bddb6c33
nikki93 <s.nikhilesh@gmail.com>
2016-12-30 01:12:49 -0800

    chapter 7: try enfocus events

--- cheshire-cat/src-cljs/core.cljs
+++ cheshire-cat/src-cljs/core.cljs
@@ -3,7 +3,8 @@
   (:require [clojure.browser.repl :as repl]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
-            [enfocus.core :as ef]))
+            [enfocus.core :as ef]
+            [enfocus.events :as ev]))
 
 (defn ^:export init []
   (repl/connect "http://localhost:9000/repl")
@@ -12,4 +13,5 @@
       (ef/at "#cat-name" (ef/content name)
              "#status" (ef/do->
                         (ef/content status)
-                        (ef/set-style :font-size "500%"))))))
+                        (ef/set-style :font-size "500%")))
+      (ef/at "#button1" (ev/listen :click #(js/alert "bye!"))))))
