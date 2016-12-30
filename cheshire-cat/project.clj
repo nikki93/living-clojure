e9f7adf2ca1736ee9440847c0bc51f35880dae53
nikki93 <s.nikhilesh@gmail.com>
2016-12-29 20:32:42 -0800

    chapter 7: get ready for client-side ClojureScript

--- cheshire-cat/project.clj
+++ cheshire-cat/project.clj
@@ -2,12 +2,24 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
-                 [ring/ring-json "0.3.1"]]
-  :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler cheshire-cat.handler/app}
+                 [ring/ring-json "0.3.1"]
+                 [org.clojure/clojurescript "0.0-2371"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.0.3"]]
+
+  :ring
+  {:handler cheshire-cat.handler/app}
+
+  :cljsbuild
+  {:builds [{:source-paths ["src-cljs"]
+             :compiler {:output-to "resources/public/main.js"
+                        :optimizations :whitespace
+                        :pretty-print true}}]}
+
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
