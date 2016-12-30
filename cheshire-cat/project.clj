9d00ba5554c544553d87f604f48d3b0ba0a6905f
nikki93 <s.nikhilesh@gmail.com>
2016-12-30 00:45:14 -0800

    chapter 7: get response from server with `cljs-http`

--- cheshire-cat/project.clj
+++ cheshire-cat/project.clj
@@ -7,7 +7,9 @@
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.3.1"]
-                 [org.clojure/clojurescript "0.0-2371"]]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [cljs-http "0.1.18"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :plugins [[lein-ring "0.9.7"]
             [lein-cljsbuild "1.0.3"]]
 
