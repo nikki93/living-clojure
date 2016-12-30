3cc05bea1248808c0d4d867c71abb84d171c90d5
nikki93 <s.nikhilesh@gmail.com>
2016-12-30 01:16:51 -0800

    chapter 7: use enfocus effects for fading out

--- cheshire-cat/src-cljs/core.cljs
+++ cheshire-cat/src-cljs/core.cljs
@@ -4,7 +4,14 @@
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
             [enfocus.core :as ef]
-            [enfocus.events :as ev]))
+            [enfocus.events :as ev]
+            [enfocus.effects :as ee]))
+
+(defn say-goodbye []
+  (ef/at
+   "#cat-name" (ee/fade-out 500)
+   "#button1" (ee/fade-out 500)
+   "#status" (ee/fade-out 5000)))
 
 (defn ^:export init []
   (repl/connect "http://localhost:9000/repl")
@@ -14,4 +21,4 @@
              "#status" (ef/do->
                         (ef/content status)
                         (ef/set-style :font-size "500%")))
-      (ef/at "#button1" (ev/listen :click #(js/alert "bye!"))))))
+      (ef/at "#button1" (ev/listen :click say-goodbye)))))
